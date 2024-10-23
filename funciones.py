9e3bee13950419760489fe72682272ce245d2072
unknown <albertfabianrincon@gmail.com>
2024-10-22 21:21:50 -0500

    funciones iniciales

---
+++ funciones.py
@@ -0,0 +1,15 @@
+
+
+class Funciones:
+    
+    def is_healthy(calories: int, vegetarian: bool):
+        if calories < 100 or vegetarian == True:
+            return True
+        else:
+            return False
+    
+    def calorie_count(calorie_list: list):
+        
+        
+            
+        
